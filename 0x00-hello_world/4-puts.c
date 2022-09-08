7756590bae53db249ba98d00932cb3f2345ee122
stebonicacid <stebonicacid@gmail.com>
2022-09-08 23:35:27 +0000

    using puts function instead of printf function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#include <stdio.h>
+ 
+/* main serves as the entry point function in C */
+ /* return always turns to zero if there are no errors else it gives some value */
+
+int main ()
+	{
+	/* puts variable prints the value inside the inverted commas. */
+		/* the \n after the variable indicates anotherline after the printed value */
+	puts ( "\"Programming is like building a multilingual puzzle" );
+	return 0;
+	}  
