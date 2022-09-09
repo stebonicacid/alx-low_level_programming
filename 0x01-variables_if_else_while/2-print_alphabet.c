7f32b01fa02fd4c321e36eca4264f87faf480931
stebonicacid <stebonicacid@gmail.com>
2022-09-09 12:20:19 +0000

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,15 +7,12 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-	for (low = 'A'; low <= 'Z'; low++)
-		putchar(low);
+	putchar(low);
 	putchar('\n');
 
 	return (0);
