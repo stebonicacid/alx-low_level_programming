345df816f37918c46c11d2aabc0d374f403ca9ae
stebonicacid <stebonicacid@gmail.com>
2022-09-09 00:21:29 +0000

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
-/*main serves as the entry point function in C*/
-/*return is always zero if there are no errors else it gives some value*/
-/*puts variable prints the value inside the inverted commas.*/
-/*the \n after the variable indicates anotherline after the printed value*/
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's prints a value by using the put variable'
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
