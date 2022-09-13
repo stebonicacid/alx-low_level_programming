4b6d29c01eb1428ac1b202df272728432aca4275
stebonicacid <stebonicacid@gmail.com>
2022-09-13 22:22:24 +0000

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_alphabet - function to print abc
+*Retrun : 0
+*/
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
