217b5be876cfb66f65105192134e3bb388d59ac9
stebonicacid <stebonicacid@gmail.com>
2022-09-14 02:54:05 +0000

    a

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,20 +10,23 @@
  */
 int print_sign(int n)
 {
-	int n;
+	int value;
 
 	if (n > 0)
 	{
+		value = 1;
 		_putchar('+');
 	}
 	else if (n == 0)
 	{
+		value = 0;
 		_putchar('0');
 	}
 	else
 	{
+		value = -1;
 		_putchar('-');
 	}
 
-	return (1);
+	return (value);
 }
