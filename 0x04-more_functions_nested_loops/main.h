0ef13767be66cbfd78bccc26946e9ea6ed1e02fa
stebonicacid <stebonicacid@gmail.com>
2022-09-15 17:20:09 +0000

    a

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,46 +1,31 @@
 int print_sign(int n);
-#ifndef main_h_included
-#define main_h_included
+#ifndef MAIN_H_included
+#define MAIN_H_included
 
-int _putchar(char);
+void print_most_numbers(void);
+void print_times_table(int n);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-void print_times_table(int n);
+void more_numbers(void);
 void num(int r, int n);
-
-#endif
-
-
-#ifndef main_h
-#define main_h
-
-/*
- * File: main.h
- * Auth: Prince Yawson
- * Desc: Header file containing declarations for all functions
- *       used in the 0x02-functions_nested_loops directory.
- */
-
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
+void print_numbers(void);
+void print_number(int n);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int);
+int print_last_digit(int);
+int add(int a, int b);
+int _isupper(int c);
+int _isdigit(int);
+int _putchar(char);
+int mul(int, int);
+
 
 #endif
