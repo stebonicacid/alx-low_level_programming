238f32b34e62097179bfd6d71026acd3d5cb7326
stebonicacid <stebonicacid@gmailcom>
2022-09-16 06:00:02 +0000

    a

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,37 +1,3 @@
-git#ifndef MAIN_H_included
-#define MAIN_H_included
-
-void print_most_numbers(void);
-void print_times_table(int n);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-void more_numbers(void);
-void num(int r, int n);
-void jack_bauer(void);
-void times_table(void);
-void print_to_98(int n);
-void print_numbers(void);
-void print_number(int n);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_integer(int n)
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int);
-int print_last_digit(int);
-int print_sign(int n);
-int add(int a, int b);
-int _isupper(int c);
-int _isdigit(int);
-int _putchar(char);
-int mul(int, int);
-
-
-#endif
-
-
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
